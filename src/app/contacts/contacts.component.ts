56ec07b9dc84fc3957baa0f19addf81ea7833fa7
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2023-01-15 20:21:33 -0300

    Components set up. Header component html.

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Contact } from './contact.model';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent {
+  contacts: Contact[] = []
+
+}
