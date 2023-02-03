7ffc1b61cc63b5f5af021088d43eea1bee3f5853
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2023-02-03 13:57:45 -0300

    Implement the Document Service

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DocumentService } from '../document-service.service';
 import { Document } from '../document.model';
 
 @Component({
@@ -7,15 +8,15 @@ import { Document } from '../document.model';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
-  documents: Document[] = [
-    new Document('1', 'Doc n1', 'Description of document number 1 here', 'https://wikipedia.com', 'docs3892, doc324'),
-    new Document('2', 'Doc n2', 'Description of document number 2 here', 'https://wikipedia.com', 'docs4352, doc678'),
-    new Document('3', 'Doc n3', 'Description of document number 3 here', 'https://wikipedia.com', 'docs36542, doc865'),
-    new Document('4', 'Doc n4', 'Description of document number 4 here', 'https://wikipedia.com', 'docs36592, doc8549'),
-    new Document('5', 'Doc n5', 'Description of document number 5 here', 'https://wikipedia.com', 'docs546, doc543')
-  ]
+  documents: Document[] = [];
+
+  constructor(private documentService: DocumentService) {}
 
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
 
   onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
---
+++ src/app/documents/document-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DocumentService {
+  documents: Document[] = [];
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments() {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string) {
+    this.documents.find((document) => {
+      return document.id = id;
+    })
+  }
+}
