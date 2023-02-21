18cce2997a29ac94d1a649fd835205dabfe67741
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2023-02-21 20:21:56 -0300

    Create and Subscribe to a Subject to Update Documents List

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DocumentService } from '../document.service';
 import { Document } from '../document.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-list',
@@ -9,14 +10,18 @@ import { Document } from '../document.model';
 })
 export class DocumentListComponent {
   documents: Document[] = [];
+  subscription!: Subscription;
 
   constructor(private documentService: DocumentService) {}
 
   ngOnInit() {
     this.documents = this.documentService.getDocuments();
-    this.documentService.documentChangedEvent.subscribe(
-      (docList: Document[]) => {
-        this.documents = docList;
+    this.subscription = this.documentService.documentListChangedEvent.subscribe((documentsList: Document[]) => {
+      this.documents = documentsList;
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
